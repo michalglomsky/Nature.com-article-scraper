6f7a33dface8c3fe568ef292c0c43d8e2538c45f
Michal Glomski <132297566+michalglomsky@users.noreply.github.com>
2025-01-09 09:45:16 +0100

    WEB SCRAPER V2
    Parsing a web page using beautifulsoup4 library. Taking nature.com article pages as valid inputs. Printing out the title and description of a page from <head> or and error message.

--- scraper.py
+++ scraper.py
@@ -1,19 +1,41 @@
 import requests
+from bs4 import BeautifulSoup
 
 # Taking the URL from the input
 url = input('Input the URL:\n> ')
 
-# Setting headers to accept JSON format
-headers = {'Accept': 'application/json'}
+# Checking the validity of the page
+if "nature.com" not in url or "articles" not in url:
+    print("Invalid page!")
+    exit()
 
 # Creating a response object from the URL
-response = requests.get(url, headers=headers) # Added the headers
+# Accepting only English pages
+r = requests.get(url, headers={'Accept-Language': 'en-US,en;q=0.5'})
+# Parsing the page
+soup = BeautifulSoup(r.content, 'html.parser')
 
-json_data = response.json()
-# Creating a conditional output
-if json_data['status']<300:
-    # Parse the JSON response
-    print(json_data['joke'])
-else:
-    # Printing the error message
-    print('Invalid resource!')
+# Initializing the output array
+article = {"title": "", "description": ""}
+# Initializing <head>
+tag_head = soup.find('head')
+
+# Checking if the article has <head>
+if tag_head:
+    # Looking for and checking if there is valid <title>
+    tag_title = tag_head.find('title')
+    if tag_title:
+        article["title"] = tag_title.text
+    else:
+        print("Invalid page!")
+        exit()
+    # Looking for and checking if there is valid <meta name="description">
+    tag_meta_description = tag_head.find('meta', {"name": "description"})
+    if tag_meta_description:
+        article["description"] = tag_meta_description["content"]
+    else:
+        print("Invalid page!")
+        exit()
+
+# Printing article dictionary with "title" and "description"
+print(article)
