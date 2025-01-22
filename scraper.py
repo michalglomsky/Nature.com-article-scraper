86c39cbd9bc81a49e900dc134ed2504effb12dc2
Michal Glomski <132297566+michalglomsky@users.noreply.github.com>
2025-01-22 13:19:30 +0100

    WEB SCRAPER V4
    A draft of articles search and saving to text files:
    - Given URL of a page and article type, find specific tags in the html of a page to identify articles of a given type
    - For all such articles:
        - Find the headline of the article and format it properly to use it as a name of a text file
        - Find the body of the article and save it to a text file with its formatted headline as a name

--- scraper.py
+++ scraper.py
@@ -1,12 +1,12 @@
 import requests
+import string
+from bs4 import BeautifulSoup
 from http import HTTPStatus
 
-# Taking the URL from the input
-input_url = input('Input the URL:\n> ')
+"""# Taking the URL from the input
+input_url = input('Input the URL:\n> ')"""
 
-# Opening the file for saving content of the page from the URL
-# Setting mode to writing in bytes
-source = open('source.html', 'wb')
+input_url = "https://www.nature.com/nature/articles?sort=PubDate&year=2020&page=3"
 
 # Getting the response message form the page
 page = requests.get(input_url)
@@ -17,8 +17,52 @@ if page.status_code != HTTPStatus.OK:
     print("The URL returned " + str(page.status_code) + "!")
     exit()
 
-# Writing the content of the page into the source.html
-source.write(page.content)
+# Creating soup for our page
+soup = BeautifulSoup(page.content, 'html.parser')
+# Finding all <article> tags
+articles_soup = soup.find_all('article')
 
-# Printing a success message
-print("Content saved.")
+# Initializing an empty dictionary of articles
+articles = []
+
+# Loop for finding right articles and extracting information about them
+for a in articles_soup:
+    # Looking for article type "News" inside <span> tag
+    article_type = a.find('span', {'data-test': 'article.type'})
+    if article_type.text == "News":
+
+        "Finding the headline, opening the file and formatting it's name"
+        # Finding the headline in <h3> tag
+        headline = a.find('h3', {'itemprop': 'name headline'}).text
+        # Sanitize the headline for file naming
+        headline = headline.strip()  # Remove leading and trailing whitespace, including \n
+        # Removing punctuation and whitespaces between words
+        for char in headline:
+            if char in string.punctuation:
+                headline = headline.replace(char, '')
+            if char == " ":
+                headline = headline.replace(char, '_')
+        # Adding file format
+        headline += '.txt'
+
+        # Opening the file and naming it
+        file = open(headline, "w", encoding="utf-8")
+        # Keeping track of the article names in the 'articles' array
+        articles.append(headline)
+
+        "Finding the article body and writing it into the file"
+        # Finding proper <a> tag to access the article body
+        article_hyperlink = a.find('a', {'data-track-action' : 'view article'})
+        # Entering the hyperlink to extract the content from the article body
+        link = "https://www.nature.com/nature" + article_hyperlink['href']
+        # Sending a request to the link
+        article_body = requests.get(link)
+        # Creating soup to extract the content of the article
+        soup_body = BeautifulSoup(article_body.content, 'html.parser')
+        # Extracting the content
+        content = soup_body.find('p', {"class": "article__teaser"}).text
+        # Writing the content into the file
+        file.write(content)
+        file.close()
+
+print(articles)
